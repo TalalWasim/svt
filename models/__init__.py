6912abcbd9cf8fd030d702902a8290df68cec64f
TalalWasim <wasimtalal@gmail.com>
2022-08-02 13:33:59 +0400

    updated model init file

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,5 @@
 from .timesformer import get_vit_base_patch16_224, get_aux_token_vit
+from .timesformer_masked import get_masked_vit_base_patch16_224
+from .timesformer_dropped import get_dropped_vit_base_patch16_224
 from .swin_transformer import SwinTransformer3D
 from .s3d import S3D
