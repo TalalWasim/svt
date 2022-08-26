3e3d45d17c09e79cb8b053f5d4acc95b148aff74
TalalWasim <wasimtalal@gmail.com>
2022-08-26 18:54:33 +0400

    update

--- models/timesformer.py
+++ models/timesformer.py
@@ -209,6 +209,31 @@ class PatchEmbed(nn.Module):
         return x, T, W
 
 
+class VideoPatchEmbed3D(nn.Module):
+    """ Image to Patch Embedding
+    """
+    def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768, num_frames=16, tubelet_size=2):
+        super().__init__()
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
+        self.tubelet_size = int(tubelet_size)
+        num_patches = (img_size[1] // patch_size[1]) * (img_size[0] // patch_size[0]) * (num_frames // self.tubelet_size)
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.num_patches = num_patches
+        self.proj3D = nn.Conv3d(in_channels=in_chans, out_channels=embed_dim, 
+                            kernel_size = (self.tubelet_size,  patch_size[0],patch_size[1]), 
+                            stride=(self.tubelet_size,  patch_size[0],  patch_size[1]))
+
+    def forward(self, x, **kwargs):
+        B, C, T, H, W = x.shape
+        # FIXME look at relaxing size constraints
+        assert H == self.img_size[0] and W == self.img_size[1], \
+            f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
+        x = self.proj3D(x).flatten(2).transpose(1, 2)
+        return x, T//self.tubelet_size, W//self.patch_size
+
+
 class VisionTransformer(nn.Module):
     """ Vision Transformer"""
 
